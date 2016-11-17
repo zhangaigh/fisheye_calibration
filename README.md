6a10da2d61c256a7ca644958adf5544ad5cb4d9f
bo zhang <zhangbo24@lenovo.com>
2016-11-17 18:35:04 +0800

    Add test data

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # fisheye_calibration
+
+# build procedure
+mkdir build
+cd build
+ccmake ../
+make
+
+# run demo
+ ./fisheye_cali -i ../data/images/ -p img
---
+++ build/CMakeFiles/fisheye_cali.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fisheye_cali.dir/calibrate_demo.cpp.o"
+  "fisheye_cali.pdb"
+  "fisheye_cali"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fisheye_cali.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
